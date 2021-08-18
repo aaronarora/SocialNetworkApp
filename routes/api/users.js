93f694b942b0eb30d188f981ee70b97fa7974bc8
Aaron Arora <aaronarora14@gmail.com>
2021-08-18 01:01:03 -0500

    moving onto next stage of endpoints

---
+++ routes/api/users.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+//at route  GET api/auth
+//@desc     test route
+//@access   Public
+//Do you need a token to access that route? Unauthorized or authorized access
+router.get("/", (req, res) => res.send("Users route"));
+
+module.exports = router;
