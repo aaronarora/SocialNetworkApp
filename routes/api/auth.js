9974dfe88a945a7af47ec3a091242de58db97cc0
Aaron Arora <aaronarora14@gmail.com>
2021-08-18 23:34:38 -0500

    fixed issue

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const auth = require("../../middleware/auth");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator/check"); //might need to remove /check
 
 const User = require("../../models/User");
 
-//at route  GET api/auth
-//@desc     test route
+//at route  POST api/auth
+//@desc    Authenticate user and get token
 //@access   Public
 //Do you need a token to access that route? Unauthorized or authorized access
 router.get("/", auth, async (req, res) => {
@@ -18,5 +22,61 @@ router.get("/", auth, async (req, res) => {
   }
 }); //when you want to use middleware we will
 //add it as a second parameter so I added auth
+
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists()
+  ],
+  async (req, res) => {
+    //console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() }); //Bad request because info was not included
+    }
+    const { email, password } = req.body; //destructure and pull out name email password
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password); // comparing plain text pass and
+      //one in databse
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+        //keep security in mind
+      }
+
+      const payload = {
+        user: {
+          id: user.id //mongoose allows for no underscore
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 }, //optional expiration
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+
+    //Return jsonwebstoken
+  }
+);
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth"); //bring in and add a 2nd parameter to protect info
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
-//at route  GET api/profile
-//@desc     test route
-//@access   Public
+//at route  GET api/profile/me
+//@desc     get current users profile
+//@access   Private
 //Do you need a token to access that route? Unauthorized or authorized access
-router.get("/", (req, res) => res.send("Profile route"));
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      "name",
+      "avatar"
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
